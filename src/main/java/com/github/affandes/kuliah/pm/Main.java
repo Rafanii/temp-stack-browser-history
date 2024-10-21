f1517743aaec24a9af7c34d76dc8440de66b0fe3
Rafanii <12350120316@students.uin-suska.ac.id>
2024-10-21 23:13:59 +0700

    Update Main.java

--- src/main/java/com/github/affandes/kuliah/pm/Main.java
+++ src/main/java/com/github/affandes/kuliah/pm/Main.java
@@ -1,8 +1,57 @@
 package com.github.affandes.kuliah.pm;
 
-public class Main {
-    public static void main(String[] args) {
-        // Tulis kode disini
+import java.util.LinkedList;
 
+public class BrowserHistory {
+    private LinkedList<String> history;
+
+    public BrowserHistory() {
+        history = new LinkedList<>();
+    }
+
+    public void view() {
+        if (history.isEmpty()) {
+            System.out.println("History browser kosong.");
+            return;
+        }
+        System.out.println("History Browser (dari yang terbaru):");
+        for (int i = history.size() - 1; i >= 0; i--) {
+            System.out.println(history.get(i));
+        }
+    }
+
+    public void browse(String website) {
+        history.add(website);
+        System.out.println("Mengunjungi: " + website);
+    }
+
+    public void back() {
+        if (history.isEmpty()) {
+            System.out.println("Tidak ada history untuk kembali.");
+            return;
+        }
+        String lastVisited = history.removeLast();
+        System.out.println("Kembali dari: " + lastVisited);
+    }
+
+    public static void main(String[] args) {
+        BrowserHistory browserHistory = new BrowserHistory();
+
+        browserHistory.browse("www.youtube.com");
+        browserHistory.browse("www.canva.com");
+        browserHistory.browse("www.yahoo.com");
+
+        browserHistory.view();
+
+        browserHistory.back();
+        browserHistory.view();
+
+        browserHistory.back();
+        browserHistory.view();
+
+        browserHistory.back();
+        browserHistory.view();
+
+        browserHistory.back();
     }
 }
